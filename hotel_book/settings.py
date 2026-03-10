8ef75bbab04f4aac0e8e7cd40dc3f764a835241f
Ragib Yeasir <ragib0072020@gmail.com>
2026-03-10 17:47:04 +0600

    Icot

--- hotel_book/settings.py
+++ hotel_book/settings.py
@@ -263,8 +263,8 @@ SSL_COMMERZ = {
     "ISSANDBOX": True,  
 }
 
-BACKEND_URL = "https://final-dusky-seven.vercel.app/"
-FRONTEND_URL = "https://full-stack-h4j3.vercel.app/"
+BACKEND_URL = "https://final-dusky-seven.vercel.app"
+FRONTEND_URL = "https://full-stack-h4j3.vercel.app"
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
